b1da7423c147fdc87b7fca5fdb6ff12baff04c5f
Alex <aov2orom@yandex.ru>
2023-06-03 17:22:11 +0300

    HW7

---
+++ src/WH7/Hero.java
@@ -0,0 +1,15 @@
+package WH7;
+
+public abstract class Hero {
+    private String name;
+    private int damage;
+    private int health;
+
+    public Hero(String name, int damage, int health) {
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+    }
+
+
+}
---
+++ src/WH7/Warriors/Warrior.java
@@ -0,0 +1,10 @@
+package WH7.Warriors;
+
+import WH7.Hero;
+
+public interface Warrior {
+    void attack(Hero target);
+
+    void shot(Hero target);
+
+}
